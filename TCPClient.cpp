ba0eb31425113f38d6e68a08ca0d194aa4e8f2db
tungmtp <75711916+tungmtp@users.noreply.github.com>
2022-05-07 22:48:28 +0700

    Add files via upload

---
+++ TCPClient.cpp
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable:4996)
+int main(int argc, char* argv[])
+{
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+    SOCKET client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(9000);
+    system("pause");
+    int ret = connect(client, (SOCKADDR*)&addr, sizeof(addr));
+    if (ret == SOCKET_ERROR)
+    {
+        ret = WSAGetLastError();
+        printf("Ket noi khong thanh cong - %d\n", ret);
+        return 1;
+    }
+    //nhận xâu chào Server gửi
+    char buffer[256];
+
+    while (true) {
+
+        int retHello = recv(client, buffer, sizeof(buffer), 0);
+        if (retHello < 0) {
+            break;
+        }
+        buffer[retHello] = 0;
+        printf("%s\n", buffer);
+        break;
+    }
+
+    //nhập xâu và gửi cho server
+    char buf[256];
+
+    while (1)
+    {
+        printf("Nhap xau ky tu: ");
+        fgets(buf, sizeof(buf), stdin);
+
+        if (strncmp(buf, "exit", 4) == 0)
+            break;
+
+        send(client, buf, strlen(buf), 0);
+    }
+
+    closesocket(client);
+    WSACleanup();
+}
---
+++ TCPServer.cpp
@@ -0,0 +1,70 @@
+﻿#include <stdio.h>
+#include <winsock2.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable:4996)
+
+int main()
+{
+    // Khoi tao thu vien
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    // Tao socket
+    SOCKET listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    // Khai bao dia chi server
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(9000);
+
+    int ret;
+    // Gan cau truc dia chi voi socket
+    bind(listener, (SOCKADDR*)&addr, sizeof(addr));
+
+    // Chuyen sang trang thai cho ket noi
+    listen(listener, 5);
+
+    SOCKET client = accept(listener, NULL, NULL);
+
+    //gửi file chào cho client
+    char text[256];
+    FILE* file_hello = fopen("C:/Users/Admin/source/repos/file_hello.txt","r");
+    while (!feof(file_hello)) {
+        ret = fread(text, 1, sizeof(text), file_hello);
+        if (ret > 0)
+        {
+            send(client, text, ret, 0);
+        }
+    }
+    fclose(file_hello);
+    // ghi nội dung nhận được vào 1 file khác
+    FILE* file = fopen("C:/Users/Admin/source/repos/file_inputFromServer.txt","a");
+    char buf[256];
+    while (1)
+    {
+        ret = recv(client, buf, sizeof(buf), 0);
+        if (ret <= 0)
+            break;
+
+        // Them ky tu ket thuc xau va in ra man hinh
+        buf[ret] = 0;
+        printf("du lieu nhan dc: %s", buf);
+
+        // Writing on file
+        fwrite(buf, 1, ret, file);
+       
+
+    }
+    fclose(file);
+
+    // Dong ket noi va giai phong thu vien
+    closesocket(client);
+    closesocket(listener);
+    WSACleanup();
+}
